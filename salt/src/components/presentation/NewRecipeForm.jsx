70fd6ddaa9c3e89319cec397b456a7b8afac58a4
github username <github email address>
2019-03-29 10:05:56 +0000

    recipe form

--- salt/src/components/presentation/NewRecipeForm.jsx
+++ salt/src/components/presentation/NewRecipeForm.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Button, Input, Form, Select} from 'semantic-ui-react'
 
 const CATEGORY = [
   "Produce",
@@ -28,7 +27,7 @@ class NewRecipeForm extends Component {
       )
     ) {
       let ingredients = [...this.state.ingredients];
-      ingredients[event.target.id][event.target.name] =
+      ingredients[event.target.dataset.id][event.target.name] =
         event.target.value;
       this.setState({ ingredients });
     } else {
@@ -60,78 +59,55 @@ class NewRecipeForm extends Component {
     this.props.addNewRecipe(newRecipe);
   };
 
-  ingredientOptions = () => { 
-    return this.props.ingredients.map((ing, i) => console.log("option", ing) ({
-      key: i,
-      value: ing.name
-    }))
-  }
-
-  unitOptions = () => { 
-    return UNITS.map((unit, i) => ({
-      key: i,
-      text: unit,
-      value: unit,
-    }))
-  }
-
-  categoryOptions = () => { 
-    return CATEGORY.map((category, i) => ({
-      key: i,
-      text: category,
-      value: category,
-    }))
-  }
-
   render() {
     return (
-      <Form className="text-container" onSubmit={this.handleFormSubmit} onChange={this.handleFieldChange} >
-       <h1>New Recipe</h1>
-        <Form.Group>
-          <Form.Input
-            
+      <div className="recipe-form-cont">
+        <h1>New Recipe</h1>
+        <form
+          onSubmit={this.handleFormSubmit}
+          onChange={this.handleFieldChange}
+        >
+          <input
+            fluid
             label="Name"
             placeholder="Name"
             name="name"
             value={this.state.name}
           />
-          <Form.Input
+          <br />
+          <input
             type="url"
-            
+            fluid
             label="image"
             placeholder="Image"
             name="image"
             value={this.state.image}
           />
-          <Form.Input
-            
+          <br />
+          <textarea
+            fluid
             label="instructions"
             placeholder="Instructions"
             name="instructions"
             value={this.state.instructions}
           />
-        </Form.Group>
-          
+          <br />
 
-        <h2>Ingredients</h2>
-        <Form.Group>
+          <h2>Ingredients</h2>
           {this.state.ingredients.map((ingredient, index) => {
             return (
               <div key={index}>
                 {/* ingredient name  */}
-                {/* <Form.Input list="ingredient_name" data-id={index} name="ingredient_name" /> */}
-
-                {/* <Form.Dropdown name="ingredient_name" id={index} search selection options={this.ingredientOptions()}/> */}
                 <input
                   list="ingredient_name"
-                  id={index}
+                  data-id={index}
                   name="ingredient_name"
                   //   value={ingredient.ingredient_name}
                 />
                 <datalist
                   id="ingredient_name"
                   name="ingredient_name"
-                  id={index}
+                  data-id={index}
                 >
                   {this.props.ingredients.map(ing => (
                     <option value={ing.name} />
@@ -143,7 +119,7 @@ class NewRecipeForm extends Component {
                 <input
                   type="number"
                   fluid
-                  id={index}
+                  data-id={index}
                   label="Qty"
                   placeholder="Quantity"
                   name="qty"
@@ -152,7 +128,7 @@ class NewRecipeForm extends Component {
 
                 {/* ingredient units */}
                 {/* <input list="units" value={ingredient.unit} /> */}
-                <select  name="unit" id={index}>
+                <select id="units" name="unit" data-id={index}>
                   <option value="" disabled selected>
                     units
                   </option>
@@ -163,24 +139,25 @@ class NewRecipeForm extends Component {
 
                 {/* ingredient ingredient category */}
 
-                <select name="ingredient_category" id={index}>
+                <select name="ingredient_category" data-id={index}>
                   <option value="" disabled selected>
                     Ingredient category
                   </option>
                   {CATEGORY.map(category => (
                     <option value={category}>{category}</option>
                   ))}
-</select>
+                </select>
 
-                  <Button onClick={() => this.removeIngredient(index)}>
-                   Remove
-                  </Button>
-                  <Form.Field onClick={this.addIngredient} control={Button}>Add Ingredient</Form.Field>
+                <button onClick={() => this.removeIngredient(index)}>
+                  Remove
+                </button>
               </div>
-             )})}          
-           </Form.Group>
-           <Form.Field control={Button}>Submit</Form.Field>
-       </Form>
+            );
+          })}
+          <button onClick={this.addIngredient}>Add Ingredient</button>
+          <button className="recipe_form_submit">Submit</button>
+        </form>
+      </div>
     );
   }
 }
